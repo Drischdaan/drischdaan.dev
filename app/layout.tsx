34e2ebd6015b5f9cbf1a7522caec1894de292ec7
Drischdaan <lpboy100@gmail.com>
2023-01-26 19:34:12 +0100

    chore: adds console easter egg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import ConsoleEasterEgg from "./components/ConsoleEasterEgg";
+
 export default function RootLayout({
   children,
 }: {
@@ -6,6 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
+      <ConsoleEasterEgg/>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import './globals.scss';
+
 import NavigationHeader from './components/navigation/NavigationHeader';
 import ContactSection from './components/sections/contact/ContactSection';
 import HomeSection from './components/sections/home/HomeSection';
-import './globals.scss';
+
+import { FiAlertTriangle } from "react-icons/fi";
 
 export default function Home() {
   return (
@@ -11,6 +14,13 @@ export default function Home() {
       {/* Sections */}
       <HomeSection/>
       <ContactSection/>
+
+      <div className='fixed bottom-0 left-0 w-full pb-5 pointer-events-none flex justify-center items-center'>
+        <div className='rounded bg-amber-400 text-black p-3 flex justify-center items-center gap-1'>
+          <FiAlertTriangle/>
+          <span>This site is under construction</span>
+        </div>
+      </div>
     </div>
   );
 }
