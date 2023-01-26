6ec070bf7dde0516bef7fc34117559492bdf1cef
Drischdaan <lpboy100@gmail.com>
2023-01-26 19:39:28 +0100

    chore: fixed naming and added ids to section

--- app/components/sections/home/HomeSection.tsx
+++ app/components/sections/home/HomeSection.tsx
@@ -2,7 +2,7 @@ import Terminal from "./Terminal";
 
 export default function HomeSection() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center dark-gradient'>
+    <div id='home' className='w-screen h-screen flex justify-center items-center dark-gradient'>
       <Terminal/>
     </div>
   );
