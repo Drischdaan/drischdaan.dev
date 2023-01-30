2f985ef26533aa12b7e09012c3694ae1dbe2828e
Drischdaan <lpboy100@gmail.com>
2023-01-30 19:55:50 +0100

    chore: adds small css improvements

--- app/components/ConsoleEasterEgg.tsx
+++ app/components/ConsoleEasterEgg.tsx
@@ -8,7 +8,7 @@ export default function ConsoleEasterEgg() {
   useEffect(() => {
     if(easterEggCalled)
       return;
-    console.log('%cYou found the easter🥚!', 'font-weight: bold; font-size: 2rem;');
+    console.log('%cYou found the easter 🥚!', 'font-weight: bold; font-size: 2rem;');
     console.log('%c🗿 %chttps://www.youtube.com/watch?v=2tRIjRVcHcQ', 'font-size: 2rem;', '');
     easterEggCalled = true;
   });
--- app/components/sections/home/HomeSection.tsx
+++ app/components/sections/home/HomeSection.tsx
@@ -3,7 +3,7 @@ import Terminal from "./Terminal";
 
 export default function HomeSection() {
   return (
-    <div id='home' className='w-screen h-screen flex justify-center items-center dark-gradient'>
+    <div id='home' className='fullscreen flex justify-center items-center dark-gradient'>
       <Terminal/>
       <div className='absolute bottom-0 left-0 w-full p-5 flex justify-center items-center pointer-events-none'>
         <a href='#about' className='pointer-events-auto animate-bounce text-4xl'>
