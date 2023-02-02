2fe27307b4face752a0825bff4fb569ce7bf0869
Drischdaan <lpboy100@gmail.com>
2023-02-02 20:48:41 +0100

    chore: started implementing new layout

---
+++ app/components/sections/home/HomeSection.tsx
@@ -0,0 +1,7 @@
+export default function HomeSection() {
+  return (
+    <div className='w-screen h-screen flex-full-center'>
+      <p>Home Section</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import './globals.scss';
 
-export default function Home() {
-  return (
-    <div>
+import HomeSection from './components/sections/home/HomeSection';
+import Taskbar, { ITaskbarItem } from './components/taskbar/Taskbar';
 
+import { FiHome } from 'react-icons/fi';
+
+export default function Home() {
+  const taskbarItems: ITaskbarItem[] = [
+    {
+      id: '#home',
+      name: 'home',
+      icon: <FiHome/>,
+    }
+  ];
+
+  return (
+    <div className='w-full h-full'>
+      <div className='fixed bottom-0 left-0 w-full'>
+        <Taskbar items={taskbarItems}/>
+      </div>
+
+      {/* Sections */}
+      <div className='w-full h-full'>
+        <HomeSection/>
+      </div>
     </div>
   );
 }
