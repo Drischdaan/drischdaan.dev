687a427a78b3a0e1a1026a5c8a68537354c89c71
Drischdaan <lpboy100@gmail.com>
2023-02-02 19:56:39 +0100

    chore: removes a lot of stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,9 @@
 import './globals.scss';
 
-import NavigationHeader from './components/navigation/NavigationHeader';
-import ContactSection from './components/sections/contact/ContactSection';
-import HomeSection from './components/sections/home/HomeSection';
-
-import { FiAlertTriangle } from "react-icons/fi";
-
 export default function Home() {
   return (
-    <div className='w-full h-full'>
-      <NavigationHeader/>
+    <div>
 
-      {/* Sections */}
-      <HomeSection/>
-      <ContactSection/>
-
-      <div className='fixed bottom-0 left-0 p-5 pointer-events-none flex justify-center items-center'>
-        <div className='rounded bg-amber-400 text-black p-3 flex justify-center items-center gap-1'>
-          <FiAlertTriangle/>
-          <span>This site is under construction</span>
-        </div>
-      </div>
     </div>
   );
 }
