9f14ebbcdb8b4e50ab0a4e179a06e291fb3d5f97
Drischdaan <lpboy100@gmail.com>
2023-02-05 19:20:54 +0100

    chore: removes current layout

--- app/components/sections/home/HomeSection.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomeSection() {
-  return (
-    <div className='w-screen h-screen flex-full-center bg-[url(https://wallpapercave.com/uwp/uwp3181483.jpeg)] bg-cover'>
-      <p className='drop-shadow-lg'>Home Section</p>
-    </div>
-  );
-}
