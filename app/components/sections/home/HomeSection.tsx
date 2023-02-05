2600b59482867bcdc6b0fb601be0c13f6c0fb43f
Drischdaan <lpboy100@gmail.com>
2023-02-05 19:16:48 +0100

    chore: adds small changes

--- app/components/sections/home/HomeSection.tsx
+++ app/components/sections/home/HomeSection.tsx
@@ -1,7 +1,7 @@
 export default function HomeSection() {
   return (
-    <div className='w-screen h-screen flex-full-center'>
-      <p>Home Section</p>
+    <div className='w-screen h-screen flex-full-center bg-[url(https://wallpapercave.com/uwp/uwp3181483.jpeg)] bg-cover'>
+      <p className='drop-shadow-lg'>Home Section</p>
     </div>
   );
 }
