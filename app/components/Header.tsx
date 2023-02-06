e65146a035c2bf312d7cf20196f14a2fb0419c28
Drischdaan <lpboy100@gmail.com>
2023-02-06 21:25:40 +0100

    chore: removes rider styled layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,29 +1,17 @@
-import Image from 'next/image';
-import { TfiClose } from 'react-icons/tfi';
-import { VscChromeMaximize, VscChromeMinimize } from 'react-icons/vsc';
 
 export default function Header() {
+  const name: string = 'drischdaan';
   return (
-    <div className='w-full flex justify-center items-center gap-3 bg-[#2B2D30]'>
-      <div className='flex-initial py-3 pl-3'>
-        <Image src='/favicon.png' width={20} height={20} alt='' />
+    <div className='w-full grid grid-cols-3 p-3'>
+      <div>
+        <div className='font-bold text-lg bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400 w-content flex justify-start items-center gap-0 relative'>
+          {name.split('').map((character, index) => (
+            <div key={index} className='hover:text-white hover:rotate-12 hover:pr-3 transition-all duration-300 cursor-pointer'>{character}</div>
+          ))}
+        </div>
       </div>
-      <div className='flex-1 py-3'>
-        <ul className='flex justify-start items-center gap-2'>
-          <li>File</li>
-          <li>Edit</li>
-        </ul>
-      </div>
-      <div className='flex-initial flex h-full'>
-        <div className='w-full h-full p-4 hover:bg-[#383A3D] transition-colors duration-200 cursor-pointer'>
-          <VscChromeMinimize/>
-        </div>
-        <div className='w-full h-full p-4 hover:bg-[#383A3D] transition-colors duration-200 cursor-pointer'>
-          <VscChromeMaximize/>
-        </div>
-        <div className='w-full h-full p-4 hover:bg-red-500 transition-colors duration-200 cursor-pointer'>
-          <TfiClose/>
-        </div>
+      <div>
+
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { AiOutlineBug, AiOutlineFolder } from 'react-icons/ai';
-import { BiDotsVerticalRounded, BiGitPullRequest, BiTerminal } from 'react-icons/bi';
-import { BsChevronDown } from 'react-icons/bs';
-import { FiAlertCircle, FiBell, FiDatabase, FiGitCommit, FiSearch } from 'react-icons/fi';
-import { IoIosGitBranch } from 'react-icons/io';
-import { IoPlayOutline, IoSettingsOutline } from 'react-icons/io5';
 import Header from './components/Header';
 import './globals.scss';
 
@@ -11,71 +5,7 @@ export default function Home() {
   return (
     <div className='w-full h-full flex flex-col'>
       <Header/>
-      <div className='w-full py-2 px-3 bg-[#2B2D30] border-y border-neutral-900 flex justify-between items-center'>
-        <div className='flex justify-start items-center gap-5'>
-          <div className='flex justify-start items-center gap-2'>
-            <span>drischdaan.dev</span>
-            <BsChevronDown/>
-          </div>
-          <div className='flex justify-start items-center gap-2'>
-            <IoIosGitBranch className='text-xl' />
-            <span>main</span>
-            <BsChevronDown/>
-          </div>
-        </div>
-        <div className='flex justify-between items-center gap-10'>
-          <div className='p-2 bg-blue-500 rounded flex justify-center items-center gap-2'>
-            Add configuration
-            <BsChevronDown/>
-            <span className='text-neutral-300'>|</span>
-            <IoPlayOutline className='text-lg'/>
-            <AiOutlineBug/>
-            <BiDotsVerticalRounded/>
-          </div>
-          <div className='flex justify-end items-center gap-5 text-xl'>
-            <FiSearch/>
-            <IoSettingsOutline/>
-          </div>
-        </div>
-      </div>
-      <div className='w-full h-full flex'>
-        <div className='flex-initial p-3 bg-[#2B2D30] border-b border-neutral-900 flex flex-col justify-between items-center'>
-          <div className='text-xl'>
-            <ul className='flex flex-col gap-3'>
-              <li><AiOutlineFolder/></li>
-              <li><FiGitCommit/></li>
-              <li><BiGitPullRequest/></li>
-            </ul>
-          </div>
-          <div className='text-xl'>
-            <ul className='flex flex-col gap-3'>
-              <li><BiTerminal/></li>
-              <li><FiAlertCircle/></li>
-            </ul>
-          </div>
-        </div>
-        <div className='flex-initial bg-[#2B2D30] border-l border-b border-neutral-900 flex flex-col'>
-          <div className='flex justify-start items-center border-b border-neutral-900 p-3 gap-2'>
-            <span>Solution</span>
-            <BsChevronDown/>
-          </div>
-        </div>
-        <div className='flex-1 p-3 bg-[#1E1F22] border border-t-0 border-neutral-900'>
 
-        </div>
-        <div className='flex-initial p-3 bg-[#2B2D30] border-b border-neutral-900 text-xl flex flex-col justify-start items-center gap-3'>
-          <FiBell/>
-          <FiDatabase/>
-        </div>
-      </div>
-      <div className='px-3 bg-[#2B2D30] flex justify-between items-center'>
-        <div></div>
-        <div className='flex justify-end items-center'>
-          <div className='p-2 bg-[#1B1C1F]'>
-            420 of 2000
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
