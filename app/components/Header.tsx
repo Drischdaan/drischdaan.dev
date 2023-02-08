5485c875faa689ec7739fd7af207da8fc809e412
Drischdaan <lpboy100@gmail.com>
2023-02-08 20:38:59 +0100

    chore: adds navigation items

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,37 @@
 
+export interface INavigationItem {
+  displayText: string;
+}
+
+export interface IHeaderProps {
+  navigationItems: INavigationItem[];
+}
+
 export default function Header() {
   const name: string = 'drischdaan';
+  const navigationItems: INavigationItem[] = [
+    { displayText: 'home', },
+    { displayText: 'about', },
+    { displayText: 'projects', },
+    { displayText: 'contact', },
+  ];
   return (
     <div className='w-full grid grid-cols-3 p-3'>
       <div>
-        <div className='font-bold text-lg bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400 w-content flex justify-start items-center gap-0 relative'>
+        <ul className='font-bold text-lg bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400 w-content flex justify-start items-center gap-0 relative'>
           {name.split('').map((character, index) => (
-            <div key={index} className='hover:text-white hover:rotate-12 hover:pr-3 transition-all duration-300 cursor-pointer'>{character}</div>
+            <li key={index} className='hover:text-white hover:rotate-12 hover:pr-3 transition-all duration-300 cursor-pointer'>{character}</li>
           ))}
-        </div>
+        </ul>
       </div>
-      <div>
-
+      <div className='flex justify-center items-center'>
+        <ul className='flex justify-center items-center gap-3'>
+          {navigationItems.map((item, index) => (
+            <li key={index}>
+              <span>{item.displayText}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
