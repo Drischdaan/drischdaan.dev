9985951d5469b386e3be9f8d7090944881d90e0d
Drischdaan <lpboy100@gmail.com>
2023-02-08 20:54:15 +0100

    chore: adds new style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import Header from './components/Header';
 import './globals.scss';
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex flex-col'>
-      <Header/>
+    <div className='w-full h-full flex bg-white'>
+      <div className='flex-1 h-full'>
 
+      </div>
+      <div className='flex-1 h-full flex flex-col justify-end items-end p-10'>
+        <div className='flex flex-col justify-end items-end font-black text-[70px] leading-none text-neutral-900'>
+          <p className='text-[120px]'>home</p>
+          <p>about</p>
+        </div>
+      </div>
     </div>
   );
 }
