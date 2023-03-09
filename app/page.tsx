0fbcfdd34715f16df466dc8430038e7e46202780
Drischdaan <lpboy100@gmail.com>
2023-03-09 20:25:31 +0100

    chore: changes layout and adds moai picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "./components/header/Header";
 import Modal from "./components/modal/Modal";
 
+import Image from "next/image";
 import { FiBriefcase, FiGlobe, FiUser } from "react-icons/fi";
 
 export default async function Home() {
   return (
-    <div className="w-full h-full">
+    <div className="h-full w-full xl:w-3/4 mx-auto">
       <Modal />
-      <div className="w-screen h-screen p-5 gap-5 grid grid-cols-1 lg:grid-cols-2 grid-rows-5 lg:grid-rows-3">
+      <div className="w-full h-screen p-5 gap-5 grid grid-cols-1 lg:grid-cols-2 grid-rows-5 lg:grid-rows-3">
         <div className="lg:col-span-2">
           <Header />
         </div>
@@ -16,7 +17,7 @@ export default async function Home() {
             <h1 className="text-5xl uppercase font-black text-black">
               drischdaan
             </h1>
-            <ul className="flex justify-start items-center gap-3 bg-black text-white font-bold uppercase py-2 px-3">
+            <ul className="flex flex-col sm:flex-row justify-start items-start sm:items-center sm:gap-3 sm:mt-0 bg-black text-white font-bold uppercase py-2 px-3">
               <li>
                 <FiBriefcase className="inline mr-1" />
                 Software Developer
@@ -32,7 +33,14 @@ export default async function Home() {
             </ul>
           </div>
         </div>
-        <div className="p-5 row-span-3 lg:row-span-1"></div>
+        <div className="p-5 row-span-3 lg:row-span-1 flex justify-center items-center">
+          <Image
+            src={"/images/moai.png"}
+            alt={"moai"}
+            width={200}
+            height={200}
+          />
+        </div>
       </div>
     </div>
   );
