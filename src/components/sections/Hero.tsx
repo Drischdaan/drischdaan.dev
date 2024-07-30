b0fbe1f24181186dd0e73b0c7b2c374d472c9ab8
Drischdaan <drischdaan@gmail.com>
2024-07-30 23:38:20 +0200

    feat: adjusting font size on big displays

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,26 +5,30 @@ export function HeroSection() {
     <section id='hero' className='relative flex h-dvh flex-col px-10'>
       <div className='relative flex flex-1 flex-col items-center justify-center'>
         <div className='mb-2 flex flex-row items-center justify-center gap-2 rounded-lg border border-zinc-600 bg-neutral-950 px-3 py-1 text-orange-200 xl:mb-3 xl:px-4'>
-          <h2 className='text-sm xl:text-base'>{`Greetings! I'm a`}</h2>
+          <h2 className='text-sm lg:text-lg xl:text-2xl'>{`Greetings! I'm a`}</h2>
         </div>
         <div className='flex flex-row items-center justify-center gap-2'>
-          <h1 className='text-zinc-500'>{'/* Hobby */'}</h1>
-          <h1 className='relative text-5xl font-semibold uppercase md:text-6xl xl:text-7xl'>
+          <h1 className='text-base text-zinc-500 lg:text-xl xl:text-3xl'>
+            {'/* Hobby */'}
+          </h1>
+          <h1 className='relative text-5xl font-semibold uppercase md:text-6xl lg:text-8xl xl:text-9xl'>
             <span className='text-lime-400'>Game</span>
             <span className='text-white'> &</span>
           </h1>
         </div>
         <div className='flex flex-row items-center justify-center gap-2'>
-          <h1 className='relative text-5xl font-semibold uppercase md:text-6xl xl:text-7xl'>
+          <h1 className='relative text-5xl font-semibold uppercase md:text-6xl lg:text-8xl xl:text-9xl'>
             <span className='text-indigo-400'>Fullstack</span>
           </h1>
-          <h1 className='text-zinc-500'>{'/* Job */'}</h1>
+          <h1 className='text-base text-zinc-500 lg:text-xl xl:text-3xl'>
+            {'/* Job */'}
+          </h1>
         </div>
-        <h1 className='relative text-5xl font-semibold uppercase text-white md:text-6xl xl:text-7xl'>
+        <h1 className='relative text-5xl font-semibold uppercase text-white md:text-6xl lg:text-8xl xl:text-9xl'>
           Developer.
         </h1>
         <div className='mt-5 xl:mt-8'>
-          <h2 className='text-center text-sm text-zinc-400'>
+          <h2 className='text-center text-sm text-zinc-400 lg:text-base xl:text-xl'>
             <span>
               Pationed about learning new{' '}
               <span className='text-indigo-400'>technologies</span> and building{' '}
@@ -33,14 +37,14 @@ export function HeroSection() {
           </h2>
         </div>
       </div>
-      <div className='absolute right-[8%] top-[15%] hidden items-center justify-center xl:right-[15%] xl:top-1/4 xl:flex'>
+      <div className='absolute right-[8%] top-[15%] hidden items-center justify-center xl:right-[15%] xl:top-[15%] xl:flex'>
         <CodeCard
           language='C++'
           code='const FEngine* Engine = new FEngine();'
           rotate='left'
         />
       </div>
-      <div className='absolute bottom-[15%] left-[10%] hidden items-center justify-center xl:bottom-1/4 xl:left-[20%] xl:flex'>
+      <div className='absolute bottom-[15%] left-[10%] hidden items-center justify-center xl:bottom-[15%] xl:left-[20%] xl:flex'>
         <CodeCard
           language='Typescript'
           code='console.log("Hello, World!");'
